c791a350223ffca326a5dd33359030fbe4421189
lsindhura <lakshmi.sindhura@rms.nsw.gov.au>
2019-11-28 17:12:45 +1100

    second commit

--- src/main/java/com/dit/batchtest/batch/BatchProcessor.java
+++ src/main/java/com/dit/batchtest/batch/BatchProcessor.java
@@ -5,27 +5,29 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class BatchProcessor implements ItemProcessor<String, String> {
 	
-	@Value("${encode.shift}")
-	private int shift;
+
+	private int shift = 3;
 	
-	String encode(String message, int offset) {
-		StringBuilder result = new StringBuilder();
-			for (char character : message.toCharArray()) {
-				if (character != ' ') {
-					int originalAlphabetPosition = character - 'a';
-					int newAlphabetPosition = (originalAlphabetPosition + offset) % 26;
-					char newCharacter = (char) ('a' + newAlphabetPosition);
-					result.append(newCharacter);
-				} else {
-					result.append(character);
-				}
-			}
-		return result.toString();
-	}
+
 
 	@Override
 	public String process(String data) throws Exception {
 		return encode(data,shift);
+	}
+
+	public String encode(String message, int offset) {
+		StringBuilder result = new StringBuilder();
+		for (char character : message.toCharArray()) {
+			if (character != ' ') {
+				int originalAlphabetPosition = character - 'a';
+				int newAlphabetPosition = (originalAlphabetPosition + offset) % 26;
+				char newCharacter = (char) ('a' + newAlphabetPosition);
+				result.append(newCharacter);
+			} else {
+				result.append(character);
+			}
+		}
+		return result.toString();
 	}
 
 }
